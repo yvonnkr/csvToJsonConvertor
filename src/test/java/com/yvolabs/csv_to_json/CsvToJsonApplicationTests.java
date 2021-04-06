3ba0ae9e0437e32d8706023de9d4956f33a53e6d
yvonnkr <34454562+yvonnkr@users.noreply.github.com>
2021-04-06 13:05:56 +0100

    Initial Commit

---
+++ src/test/java/com/yvolabs/csv_to_json/CsvToJsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yvolabs.csv_to_json;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvToJsonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
