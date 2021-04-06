ae48ab8a3226037c8dd76be643647a1416157809
yvonnkr <34454562+yvonnkr@users.noreply.github.com>
2021-04-06 13:20:17 +0100

    Added dependencies and  test files

---
+++ src/main/java/com/yvolabs/csv_to_json/Main.java
@@ -0,0 +1,12 @@
+package com.yvolabs.csv_to_json;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args)  {
+        System.out.println("hello");
+    }
+
+}
